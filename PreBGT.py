fcedb7caf19152e82d09ae2c2ced41b54096bf74
songweizhi <songwz03@gmail.com>
2020-09-01 21:09:59 +1000

    -

--- PreBGT.py
+++ PreBGT.py
@@ -22,6 +22,19 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
 
 
+def sep_path_basename_ext(file_in):
+
+    # separate path and file name
+    file_path, file_name = os.path.split(file_in)
+    if file_path == '':
+        file_path = '.'
+
+    # separate file basename and extension
+    file_basename, file_extension = os.path.splitext(file_name)
+
+    return file_path, file_basename, file_extension
+
+
 def get_aa_group_pct_dict(faa_folder, aa_group_list):
 
     faa_file_re = '%s/*.faa' % faa_folder
@@ -97,218 +110,241 @@ def matrix_to_one_col(file_in, file_out, unchanged_col_num):
     df_one_col_handle.close()
 
 
-###################################################### file in/out #####################################################
+def PreBGT(args):
 
-notes = '''
-hydrophobic amino acids: GAVLIPFMW
-'''
-wd = '/Users/songweizhi/Desktop/PreTR_ML'
+    ###################################################### file in/out #####################################################
 
-# file and parameter in
-faa_folder          = '%s/faa_files'           % wd
-genome_cate_file    = '%s/Genome_category.csv' % wd
-aa_group_list       = ['R', 'ED', 'RED', 'IVYWREL', 'GAVLIPFMW', 'NQ_NQED']
-aa_group_list       = ['R', 'ED', 'RED', 'IVYWREL', 'NQ_NQED']
-#aa_group_list       = ['R', 'ED', 'RED', 'IVYWREL', 'NQ_NQED', 'EK_QH', 'LK_Q']
+    # file and parameter in
+    faa_folder          = args['faa_folder']
+    genome_cate_file    = args['genome_cate_file']
+    aa_group_list       = args['aa_group_list']
+    genomic_traits_file = args['genomic_traits_file']
+    prepare_traits_file = args['prepare_traits_file']
+    plot_traits         = args['plot_traits']
 
-prepare_traits_file = True  # True or False
-plot_traits         = True  # True or False
-prepare_traits_file = False  # True or False
-plot_traits         = False  # True or False
+    test_size               = 0.20
+    random_state            = 1
+    n_splits                = 10
 
 
-validation_size         = 0.20
-test_size               = 0.20
-random_state            = 1
-n_splits                = 10
+    ################################################# define fileneame #################################################
 
-# file out
-genomic_traits_file                 = '%s/Genomic_traits.csv'                       % wd
-genomic_traits_OneCol               = '%s/Genomic_traits_OneCol.csv'                % wd
-genomic_traits_plot_scatter_matrix  = '%s/Genomic_traits_scatter_matrix.pdf'        % wd
-genomic_traits_plot_grouped_boxplot = '%s/Genomic_traits_grouped_boxplot.pdf'       % wd
-ml_plot_algorithm_comparison        = '%s/Genomic_traits_algorithm_comparison.pdf'  % wd
+    genomic_traits_file_path, genomic_traits_file_basename, genomic_traits_file_extension = sep_path_basename_ext(genomic_traits_file)
+
+    genomic_traits_OneCol               = '%s/%s_OneCol.csv'                % (genomic_traits_file_path, genomic_traits_file_basename)
+    genomic_traits_plot_scatter_matrix  = '%s/%s_scatter_matrix.pdf'        % (genomic_traits_file_path, genomic_traits_file_basename)
+    genomic_traits_plot_grouped_boxplot = '%s/%s_grouped_boxplot.pdf'       % (genomic_traits_file_path, genomic_traits_file_basename)
+    ml_plot_algorithm_comparison        = '%s/%s_algorithm_comparison.pdf'  % (genomic_traits_file_path, genomic_traits_file_basename)
 
 
-############################################# prepare genomic traits file ##############################################
+    ############################################# prepare genomic traits file ##############################################
 
-# read in genome cate info
-genome_cate_dict = {}
-for each_genome in open(genome_cate_file):
-    each_genome_split = each_genome.strip().split(',')
-    genome_cate_dict[each_genome_split[0]] = each_genome_split[1]
-
-
-# split aa group if found "_"
-aa_group_list_splitted = []
-for aa_group in aa_group_list:
-    if '_' in aa_group:
-        aa_group_split = aa_group.split('_')
-        if len(aa_group_split) == 2:
-            aa_group_list_splitted.append(aa_group_split[0])
-            aa_group_list_splitted.append(aa_group_split[1])
-        else:
-            print('Found more than one "_" in amino acid group, program exited!')
-            exit()
-    else:
-        aa_group_list_splitted.append(aa_group)
+    # read in genome cate info
+    genome_cate_dict = {}
+    for each_genome in open(genome_cate_file):
+        each_genome_split = each_genome.strip().split(',')
+        genome_cate_dict[each_genome_split[0]] = each_genome_split[1]
 
 
-if prepare_traits_file is True:
-
-    # get aa_group_pct_dict
-    aa_group_pct_dict = get_aa_group_pct_dict(faa_folder, aa_group_list_splitted)
-
-
-    # get faa_file_list
-    faa_file_re = '%s/*.faa' % faa_folder
-    faa_file_list = [os.path.basename(file_name) for file_name in glob.glob(faa_file_re)]
-
-
-    # prepare genomic traits file
-    genomic_traits_file_handle = open(genomic_traits_file, 'w')
-    genomic_traits_file_handle.write('GenomeID,Category,%s\n' % ','.join(aa_group_list))
-
-    for faa_file in sorted(faa_file_list):
-
-        faa_file_base_name = faa_file[:-4]
-        genome_cate = genome_cate_dict[faa_file_base_name]
-
-        aa_group_pct_list = []
-        for aa_group in aa_group_list:
-            if '_' in aa_group:
-                aa_group_numerator   = aa_group.split('_')[0]
-                aa_group_denominator = aa_group.split('_')[1]
-                aa_group_ratio = (aa_group_pct_dict[aa_group_numerator][faa_file_base_name])/(aa_group_pct_dict[aa_group_denominator][faa_file_base_name])
-                aa_group_ratio = float("{0:.4f}".format(aa_group_ratio))
-                aa_group_pct_list.append(aa_group_ratio)
-
+    # split aa group if found "_"
+    aa_group_list_splitted = []
+    for aa_group in aa_group_list:
+        if '_' in aa_group:
+            aa_group_split = aa_group.split('_')
+            if len(aa_group_split) == 2:
+                aa_group_list_splitted.append(aa_group_split[0])
+                aa_group_list_splitted.append(aa_group_split[1])
             else:
-                aa_group_pct_list.append(float("{0:.4f}".format(aa_group_pct_dict[aa_group][faa_file_base_name])))
-
-        aa_group_pct_list_str = [str(i) for i in aa_group_pct_list]
-        genomic_traits_file_handle.write('%s,%s,%s\n' % (faa_file_base_name, genome_cate, ','.join(aa_group_pct_list_str)))
-
-    genomic_traits_file_handle.close()
+                print('Found more than one "_" in amino acid group, program exited!')
+                exit()
+        else:
+            aa_group_list_splitted.append(aa_group)
 
 
-################################################# plot genomic traits ##################################################
+    if prepare_traits_file is True:
 
-# read in dataframe
-genomic_traits_df = read_csv(genomic_traits_file, header=0)
+        # get aa_group_pct_dict
+        aa_group_pct_dict = get_aa_group_pct_dict(faa_folder, aa_group_list_splitted)
 
-if plot_traits is True:
 
-    #################### get scatter_matrix plot ####################
+        # get faa_file_list
+        faa_file_re = '%s/*.faa' % faa_folder
+        faa_file_list = [os.path.basename(file_name) for file_name in glob.glob(faa_file_re)]
 
-    genome_cate_index = []
-    for i in genomic_traits_df.values[:, 1]:
-        if i == 'Mesophiles':
-            genome_cate_index.append(0)
-        elif i == 'Psychrophiles':
-            genome_cate_index.append(1)
-        elif i == 'Thermophiles':
-            genome_cate_index.append(2)
 
-    genome_cate_index_array = np.array(genome_cate_index)
+        # prepare genomic traits file
+        genomic_traits_file_handle = open(genomic_traits_file, 'w')
+        genomic_traits_file_handle.write('GenomeID,Category,%s\n' % ','.join(aa_group_list))
 
-    scatter_matrix(genomic_traits_df, c=genome_cate_index_array, marker='o', s=6)
-    plt.savefig(genomic_traits_plot_scatter_matrix)
+        for faa_file in sorted(faa_file_list):
+
+            faa_file_base_name = faa_file[:-4]
+            genome_cate = genome_cate_dict[faa_file_base_name]
+
+            aa_group_pct_list = []
+            for aa_group in aa_group_list:
+                if '_' in aa_group:
+                    aa_group_numerator   = aa_group.split('_')[0]
+                    aa_group_denominator = aa_group.split('_')[1]
+                    aa_group_ratio = (aa_group_pct_dict[aa_group_numerator][faa_file_base_name])/(aa_group_pct_dict[aa_group_denominator][faa_file_base_name])
+                    aa_group_ratio = float("{0:.4f}".format(aa_group_ratio))
+                    aa_group_pct_list.append(aa_group_ratio)
+
+                else:
+                    aa_group_pct_list.append(float("{0:.4f}".format(aa_group_pct_dict[aa_group][faa_file_base_name])))
+
+            aa_group_pct_list_str = [str(i) for i in aa_group_pct_list]
+            genomic_traits_file_handle.write('%s,%s,%s\n' % (faa_file_base_name, genome_cate, ','.join(aa_group_pct_list_str)))
+
+        genomic_traits_file_handle.close()
+
+
+    ################################################# plot genomic traits ##################################################
+
+    # read in dataframe
+    genomic_traits_df = read_csv(genomic_traits_file, header=0)
+
+    if plot_traits is True:
+
+        #################### get scatter_matrix plot ####################
+
+        genome_cate_index = []
+        for i in genomic_traits_df.values[:, 1]:
+            if i == 'Mesophiles':
+                genome_cate_index.append(0)
+            elif i == 'Psychrophiles':
+                genome_cate_index.append(1)
+            elif i == 'Thermophiles':
+                genome_cate_index.append(2)
+
+        genome_cate_index_array = np.array(genome_cate_index)
+
+        scatter_matrix(genomic_traits_df, c=genome_cate_index_array, marker='o', s=6)
+        plt.savefig(genomic_traits_plot_scatter_matrix)
+        plt.close()
+        plt.clf()
+
+        #################### get grouped_boxplot ####################
+
+        matrix_to_one_col(genomic_traits_file, genomic_traits_OneCol, 2)
+        dataset_OneCol = read_csv(genomic_traits_OneCol, header=0)
+
+        box_order   = ['Psychrophiles',   'Mesophiles', 'Thermophiles']
+        color_order = ['dodgerblue', 'orange', 'red']
+        sns.boxplot(data=dataset_OneCol, x="Var", y="Value", hue="Category", hue_order=box_order, palette=color_order)
+        plt.savefig(genomic_traits_plot_grouped_boxplot)
+
+        plt.close()
+        plt.clf()
+
+        #################### remove tmp file ####################
+
+        os.remove(genomic_traits_OneCol)
+
+
+    ################################################ Stats of genomic traits ###############################################
+
+    '''
+    
+    # turn number list into arrary
+    measurement_P_arrary = np.array(measurement_P)
+    measurement_M_arrary = np.array(measurement_M)
+    measurement_T_arrary = np.array(measurement_T)
+    
+    print('Psychrophiles\tmean: %s\tstd: %s' % (float("{0:.3f}".format(measurement_P_arrary.mean())), float("{0:.3f}".format(measurement_P_arrary.std()))))
+    print('Mesophiles\tmean: %s\tstd: %s' % (float("{0:.3f}".format(measurement_M_arrary.mean())), float("{0:.3f}".format(measurement_M_arrary.std()))))
+    print('Thermophiles\tmean: %s\tstd: %s' % (float("{0:.3f}".format(measurement_T_arrary.mean())), float("{0:.3f}".format(measurement_T_arrary.std()))))
+    print('')
+    print('Psychrophiles\tvs\tMesophiles\t(T-test):\tp=%s' % float("{0:.3f}".format(stats.ttest_ind(measurement_P, measurement_M, equal_var=False).pvalue)))
+    print('Psychrophiles\tvs\tThermophiles\t(T-test):\tp=%s' % float("{0:.3f}".format(stats.ttest_ind(measurement_P, measurement_T, equal_var=False).pvalue)))
+    print('Mesophiles\tvs\tThermophiles\t(T-test):\tp=%s' % float("{0:.3f}".format(stats.ttest_ind(measurement_M, measurement_T, equal_var=False).pvalue)))
+    
+    '''
+
+    ################################################## Compare algorithms ##################################################
+
+    # read in dataframe
+    genomic_traits_array = genomic_traits_df.values
+    genome_cate = genomic_traits_array[:, 1]
+    traits_matrix = genomic_traits_array[:, 2:]
+    traits_matrix_train, traits_matrix_validation, genome_cate_train, genome_cate_validation = train_test_split(traits_matrix, genome_cate, test_size=test_size, random_state=random_state)
+
+    # Spot Check Algorithms
+    models = []
+    models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
+    models.append(('LDA', LinearDiscriminantAnalysis()))
+    models.append(('KNN', KNeighborsClassifier()))
+    models.append(('CART', DecisionTreeClassifier()))
+    models.append(('NB', GaussianNB()))
+    models.append(('SVM', SVC(gamma='auto')))
+
+    # evaluate each model in turn
+    results = []
+    names = []
+    print('Compare algorithms:')
+    print('Algorithm\tMean\tStd')
+    for name, model in models:
+        kfold = StratifiedKFold(n_splits=n_splits, random_state=random_state)
+
+        cv_results = cross_val_score(model,
+                                     traits_matrix_train,
+                                     genome_cate_train,
+                                     cv=KFold(n_splits=n_splits, random_state=random_state),
+                                     scoring='accuracy')
+
+        results.append(cv_results)
+        names.append(name)
+        print('%s\t%f\t%f' % (name, cv_results.mean(), cv_results.std()))
+
+    # Compare Algorithms
+    plt.boxplot(results, labels=names)
+    plt.title('Algorithm Comparison')
+    plt.savefig(ml_plot_algorithm_comparison)
     plt.close()
     plt.clf()
 
-    #################### get grouped_boxplot ####################
 
-    matrix_to_one_col(genomic_traits_file, genomic_traits_OneCol, 2)
-    dataset_OneCol = read_csv(genomic_traits_OneCol, header=0)
+    ################################################ Training and Prediction ###############################################
 
-    box_order   = ['Psychrophiles',   'Mesophiles', 'Thermophiles']
-    color_order = ['dodgerblue', 'orange', 'red']
-    sns.boxplot(data=dataset_OneCol, x="Var", y="Value", hue="Category", hue_order=box_order, palette=color_order)
-    plt.savefig(genomic_traits_plot_grouped_boxplot)
+    # Make predictions
+    algorithm_chose = LinearDiscriminantAnalysis()
+    algorithm_chose.fit(traits_matrix_train, genome_cate_train)
+    genome_cate_predictions = algorithm_chose.predict(traits_matrix_validation)
 
-    plt.close()
-    plt.clf()
-
-    #################### remove tmp file ####################
-
-    os.remove(genomic_traits_OneCol)
+    print('\nPrediction results:')
+    print('Algorithm chose\tLinearDiscriminantAnalysis')
+    print('Accuracy\t%s' % accuracy_score(genome_cate_validation, genome_cate_predictions))
+    print('\nClassification_report:\n%s' % classification_report(genome_cate_validation, genome_cate_predictions))
 
 
-################################################ Stats of genomic traits ###############################################
+if __name__ == '__main__':
+
+    wd                      = '/Users/songweizhi/Desktop/PreTR_ML'
+    faa_folder              = '%s/faa_files'                                % wd
+    genome_cate_file        = '%s/Genome_category.csv'                      % wd
+    genomic_traits_file     = '%s/Genomic_traits.csv'                       % wd
+    aa_group_list           = ['R', 'ED', 'RED', 'IVYWREL', 'NQ_NQED']
+    prepare_traits_file     = True  # True or False
+    plot_traits             = True  # True or False
+
+    args = {'faa_folder'            : faa_folder,
+            'genome_cate_file'      : genome_cate_file,
+            'aa_group_list'         : aa_group_list,
+            'genomic_traits_file'   : genomic_traits_file,
+            'prepare_traits_file'   : prepare_traits_file,
+            'plot_traits'           : plot_traits
+            }
+
+    PreBGT(args)
+
 
 '''
 
-# turn number list into arrary
-measurement_P_arrary = np.array(measurement_P)
-measurement_M_arrary = np.array(measurement_M)
-measurement_T_arrary = np.array(measurement_T)
+hydrophobic amino acids: GAVLIPFMW
 
-print('Psychrophiles\tmean: %s\tstd: %s' % (float("{0:.3f}".format(measurement_P_arrary.mean())), float("{0:.3f}".format(measurement_P_arrary.std()))))
-print('Mesophiles\tmean: %s\tstd: %s' % (float("{0:.3f}".format(measurement_M_arrary.mean())), float("{0:.3f}".format(measurement_M_arrary.std()))))
-print('Thermophiles\tmean: %s\tstd: %s' % (float("{0:.3f}".format(measurement_T_arrary.mean())), float("{0:.3f}".format(measurement_T_arrary.std()))))
-print('')
-print('Psychrophiles\tvs\tMesophiles\t(T-test):\tp=%s' % float("{0:.3f}".format(stats.ttest_ind(measurement_P, measurement_M, equal_var=False).pvalue)))
-print('Psychrophiles\tvs\tThermophiles\t(T-test):\tp=%s' % float("{0:.3f}".format(stats.ttest_ind(measurement_P, measurement_T, equal_var=False).pvalue)))
-print('Mesophiles\tvs\tThermophiles\t(T-test):\tp=%s' % float("{0:.3f}".format(stats.ttest_ind(measurement_M, measurement_T, equal_var=False).pvalue)))
+    # aa_group_list = ['R', 'ED', 'RED', 'IVYWREL', 'GAVLIPFMW', 'NQ_NQED']
+    # aa_group_list = ['R', 'ED', 'RED', 'IVYWREL', 'NQ_NQED']
+    # aa_group_list = ['R', 'ED', 'RED', 'IVYWREL', 'NQ_NQED', 'EK_QH', 'LK_Q']
 
 '''
 
-################################################## Compare algorithms ##################################################
-
-# read in dataframe
-genomic_traits_array = genomic_traits_df.values
-genome_cate = genomic_traits_array[:, 1]
-traits_matrix = genomic_traits_array[:, 2:]
-traits_matrix_train, traits_matrix_validation, genome_cate_train, genome_cate_validation = train_test_split(traits_matrix, genome_cate, test_size=test_size, random_state=random_state)
-
-# Spot Check Algorithms
-models = []
-models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
-models.append(('LDA', LinearDiscriminantAnalysis()))
-models.append(('KNN', KNeighborsClassifier()))
-models.append(('CART', DecisionTreeClassifier()))
-models.append(('NB', GaussianNB()))
-models.append(('SVM', SVC(gamma='auto')))
-
-# evaluate each model in turn
-results = []
-names = []
-print('Compare algorithms:')
-print('Algorithm\tMean\tStd')
-for name, model in models:
-    kfold = StratifiedKFold(n_splits=n_splits, random_state=random_state)
-
-    cv_results = cross_val_score(model,
-                                 traits_matrix_train,
-                                 genome_cate_train,
-                                 cv=KFold(n_splits=n_splits, random_state=random_state),
-                                 scoring='accuracy')
-
-    results.append(cv_results)
-    names.append(name)
-    print('%s\t%f\t%f' % (name, cv_results.mean(), cv_results.std()))
-
-# Compare Algorithms
-plt.boxplot(results, labels=names)
-plt.title('Algorithm Comparison')
-plt.savefig(ml_plot_algorithm_comparison)
-plt.close()
-plt.clf()
-
-
-################################################ Training and Prediction ###############################################
-
-# Make predictions
-algorithm_chose = LinearDiscriminantAnalysis()
-algorithm_chose.fit(traits_matrix_train, genome_cate_train)
-genome_cate_predictions = algorithm_chose.predict(traits_matrix_validation)
-
-print('\nPrediction results:')
-print('Algorithm chose\tLinearDiscriminantAnalysis')
-print('Accuracy\t%s' % accuracy_score(genome_cate_validation, genome_cate_predictions))
-print('\nClassification_report:\n%s' % classification_report(genome_cate_validation, genome_cate_predictions))
-
-
-print(genome_cate_validation)
-print(genome_cate_predictions)
